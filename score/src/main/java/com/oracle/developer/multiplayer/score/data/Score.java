64bbb9df196f32077617b2fe581129d77cf554c3
Victor Martin <vmleon@gmail.com>
2023-03-14 13:06:23 +0100

    downgrade Spring Boot to 2.7.9 for compatibility with Cloud Shell

--- score/src/main/java/com/oracle/developer/multiplayer/score/data/Score.java
+++ score/src/main/java/com/oracle/developer/multiplayer/score/data/Score.java
@@ -1,13 +1,14 @@
 package com.oracle.developer.multiplayer.score.data;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @ToString
