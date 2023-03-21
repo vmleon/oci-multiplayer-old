afe93e8de54b8dbced8e036297d96c0522b50acb
Wojciech Pluta <60693402+WSPluta@users.noreply.github.com>
2023-03-21 15:29:57 +0000

    webpack update for local dev

--- web/bundler/webpack.dev.js
+++ web/bundler/webpack.dev.js
@@ -1,36 +1,33 @@
-const {merge} = require('webpack-merge');
-const commonConfiguration = require('./webpack.common.js');
-const ip = require('internal-ip');
-const portFinderSync = require('portfinder-sync');
+const { merge } = require("webpack-merge");
+const commonConfiguration = require("./webpack.common.js");
+const portFinderSync = require("portfinder-sync");
 
 const infoColor = (_message) => {
   return `\u001b[1m\u001b[34m${_message}\u001b[39m\u001b[22m`;
 };
 
 module.exports = merge(commonConfiguration, {
-  mode: 'development',
+  mode: "development",
   devServer: {
-    host: '0.0.0.0',
+    host: "localhost",
     port: portFinderSync.getPort(8080),
-    contentBase: './dist',
+    contentBase: "./dist",
     watchContentBase: true,
     proxy: {
-      '/socket.io': {target: 'ws://localhost:3000'},
+      "/socket.io": { target: "ws://localhost:3000" },
     },
     open: true,
     https: false,
-    useLocalIp: true,
+    useLocalIp: false,
     disableHostCheck: true,
     overlay: true,
     noInfo: true,
     after: function (app, server, compiler) {
       const port = server.options.port;
-      const https = server.options.https ? 's' : '';
-      const localIp = ip.v4.sync();
-      const domain1 = `http${https}://${localIp}:${port}`;
-      const domain2 = `http${https}://localhost:${port}`;
+      const https = server.options.https ? "s" : "";
+      const domain = `http${https}://localhost:${port}`;
 
-      console.log(`Project running at:\n  - ${infoColor(domain1)}\n  - ${infoColor(domain2)}`);
+      console.log(`Project running at:\n  - ${infoColor(domain)}`);
     },
   },
 });
