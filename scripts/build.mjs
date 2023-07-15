77c5d1de4568e230ca2d75dd0ba30ea8c70757e2
Victor Martin <vmleon@gmail.com>
2023-07-15 23:04:49 +0200

    fix oci-multiplayer as project name for DevOps

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,12 +1,7 @@
 #!/usr/bin/env zx
 import { getNpmVersion } from "./lib/npm.mjs";
 import { getNamespace, getRegionByName } from "./lib/oci.mjs";
-import {
-  checkPodmanMachineRunning,
-  buildImage,
-  tagImage,
-  pushImage,
-} from "./lib/container.mjs";
+import { checkPodmanMachineRunning, buildImage } from "./lib/container.mjs";
 import { getVersionGradle } from "./lib/gradle.mjs";
 
 const shell = process.env.SHELL | "/bin/zsh";
@@ -16,36 +11,33 @@ $.verbose = false;
 checkPodmanMachineRunning();
 
 const namespace = await getNamespace();
-console.log({ namespace });
 const ociRegionNameFromEnv = (await $`echo $OCI_REGION`).stdout.trim();
 const region = await getRegionByName(ociRegionNameFromEnv);
 const regionKey = region["region-key"].toLowerCase();
-console.log({ regionKey });
+console.log({ namespace, regionKey });
 
 const { a, _ } = argv;
-const [action, push] = _;
-
-const project = "oci_multiplayer";
+const [action] = _;
 
 if (action === "ws-server") {
-  await releaseNpm("server", push);
+  await releaseNpm("server");
   process.exit(0);
 }
 
 if (action === "web") {
-  await releaseNpm("web", push);
+  await releaseNpm("web");
   process.exit(0);
 }
 
 if (action === "score") {
-  await releaseGradle("score", push);
+  await releaseGradle("score");
   process.exit(0);
 }
 
 if (a || action === "all") {
-  await releaseNpm("server", push);
-  await releaseNpm("web", push);
-  await releaseGradle("score", push);
+  await releaseNpm("server");
+  await releaseNpm("web");
+  await releaseGradle("score");
   process.exit(0);
 }
 
@@ -56,36 +48,18 @@ console.log("\tnpx zx scripts/build.mjs ws-server");
 console.log("\tnpx zx scripts/build.mjs web");
 console.log("\tnpx zx scripts/build.mjs score");
 
-async function releaseNpm(service, push = false) {
+async function releaseNpm(service) {
   await cd(`${service}`);
   const currentVersion = await getNpmVersion();
-  console.log(
-    `Releasing ${service}:${currentVersion} (push: ${push.toString()})`
-  );
+  console.log(`Releasing ${service}:${currentVersion})`);
   await buildImage(`${service}`, currentVersion);
-  const localImage = `${service}:${currentVersion}`;
-  const remoteImage = `${regionKey}.ocir.io/${namespace}/${project}/${service}:${currentVersion}`;
-  await tagImage(localImage, remoteImage);
-  if (push) {
-    await pushImage(remoteImage);
-    console.log(`Released: ${chalk.yellow(remoteImage)}`);
-  }
   await cd("..");
 }
 
-async function releaseGradle(service, push = false) {
+async function releaseGradle(service) {
   await cd(`${service}`);
   const currentVersion = await getVersionGradle();
-  console.log(
-    `Releasing ${service}:${currentVersion} (push: ${push.toString()})`
-  );
+  console.log(`Releasing ${service}:${currentVersion})`);
   await buildImage(`${service}`, currentVersion);
-  const localImage = `${service}:${currentVersion}`;
-  const remoteImage = `${regionKey}.ocir.io/${namespace}/${project}/${service}:${currentVersion}`;
-  await tagImage(localImage, remoteImage);
-  if (push) {
-    await pushImage(remoteImage);
-    console.log(`Released: ${chalk.yellow(remoteImage)}`);
-  }
   await cd("..");
 }
